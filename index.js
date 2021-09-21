de4ed3a74118a7aa01840c6e78250b75d1687018
Stanislav <verbatim8872@gmail.com>
2021-09-21 12:59:12 +0300

    fix deprecated warning

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
+process.env.NTBA_FIX_319 = 1
+
 const TelegramApi = require('node-telegram-bot-api')
+
+
 
 const token = '2045807538:AAH5bnOdR08eGGJvZOTrW4J8PCDnec2CooU'
 
 const bot = new TelegramApi(token, { polling: true })
 
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Привет, Друг!');
-});
+const run = () => {
+    bot.on('message', async (msg) => {
+        const chatId = msg.chat.id
+        const command = msg.text
+    
+        if(command === '/info'){
+            await bot.sendMessage(chatId, `Тебя зовут ${msg.chat.first_name} ${msg.chat.last_name}!`)
+            return
+        }
+    
+        await bot.sendMessage(chatId, 'Я тебя не понимаю!')
+        return
+    })
+}
+
+run()
